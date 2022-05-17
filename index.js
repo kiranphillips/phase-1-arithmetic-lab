ca59b33c6f6b30cb81432356bc094f603d155c6e
Kiran Phillips <Kiranphillips61@gmail.com>
2022-05-17 09:24:58 -0600

    commit tree c5ca788f9e73a88fcd1aba3387d4cbc8e226e3cc

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31 
+const num2 = 2 
+const multiply = (num1 * num2)
 
-// Write your code here
+let random = Math.round((Math.random() + 1))
+
+const num3 = 6 
+const num4 = 2
+
+const mod = (num3 - num4)
+
+let max = Math.max(20)
